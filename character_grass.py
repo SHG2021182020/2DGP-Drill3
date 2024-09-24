95740e64f4275c55636f5674f37ccbfda9b66513
SHG2021182020 <96454074+SHG2021182020@users.noreply.github.com>
2024-09-24 09:54:10 +0900

    Update_2
    제대로 돌아가는지 확인, 무한 반복은 10회 제한으로 변경

--- character_grass.py
+++ character_grass.py
@@ -1,17 +1,19 @@
 from pico2d import *
 
 def run_rectangle():
+	print('RECTANGLE');
 	pass
 
 def run_circle():
+	print('CIRCLE')
 	pass
 
 open_canvas()
 
 grass = load_image('grass.png')
 boy = load_image('character.png')
-
-while True:
+i = 0
+for i in range(10):
     run_rectangle()
     run_circle()
 
